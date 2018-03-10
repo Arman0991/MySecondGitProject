c78d011bd6b62466a331b726bb7efb8668435b2b
Arman0991 <33642672+Arman0991@users.noreply.github.com>
2018-03-10 12:22:08 -0500

    added new statements

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -3,6 +3,6 @@ package main;
 public class GoodEvening {
 	public static void main(String[] args) {
 		System.out.println("Good Evening Class");
-		System.out.println("Good");
+		System.out.println("My name is ahsaboy");
 	}
 }
